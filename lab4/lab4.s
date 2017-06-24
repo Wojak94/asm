c714e13b9c51606a229f38b18cd7520b391bd02e
Wojak94 <wojciechadles@gmail.com>
2017-06-24 14:37:50 +0200

    tabulation issues

--- lab4/lab4.s
+++ lab4/lab4.s
@@ -1,7 +1,7 @@
 .section .data
 
 SYSEXIT = 60
-formatd: .asciz "%d"	
+formatd: .asciz "%d"
 formatf: .asciz "%f \n"
 decimal: .long 0
 flt: .double 0
@@ -12,22 +12,21 @@ n2: .double 2.5
 
 .section .text
 .globl _start
-_start:	
+_start:
  	movq $0, %rax    		#clear rax (passing 0 float arguments)
     	movq $formatd, %rdi    		#load format string
    	movq $decimal, %rsi    		#set storage to address of x
-    	call scanf    	
+    	call scanf
 	movq $2, %rax    		#2 floating point arguments
 	movq $0, %rdi    		#clear rdi
 	movl decimal, %edi
 	movsd n1, %xmm0
 	movsd n2, %xmm1
-	call f	
-	movsd %xmm0, flt	
+	call f
+	movsd %xmm0, flt
 	movq $1, %rax    		#one float number as argument
-    	movq $formatf, %rdi    		#load format string  			
+    	movq $formatf, %rdi    		#load format string
     	call printf
-	addq $8, %rsp	
+	addq $8, %rsp
     	movq $SYSEXIT, %rax
 	syscall				#Exiting program
-
